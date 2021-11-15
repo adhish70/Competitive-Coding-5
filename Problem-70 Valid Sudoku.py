cb6606f6aeba2c3eb86a6ba71f00f3918b447f28
Adhish Shrivastava <adhish70@gmail.com>
2021-11-15 12:35:06 -0500

    Initial Commit

---
+++ Problem-70 Valid Sudoku.py
@@ -0,0 +1,11 @@
+# 36. Valid Sudoku
+# https://leetcode.com/problems/valid-sudoku/
+
+# Logic: 
+
+# Time Complexiety: 
+# Space Complexiety: 
+
+class Solution:
+    def isValidSudoku(self, board: List[List[str]]) -> bool:
+        pass
---
+++ Problem-71 Find Largest Value in Each Tree Row.py
@@ -0,0 +1,17 @@
+# 515. Find Largest Value in Each Tree Row
+# https://leetcode.com/problems/find-largest-value-in-each-tree-row/
+
+# Logic: 
+
+# Time Complexiety: 
+# Space Complexiety: 
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:            
+    def largestValues(self, root: Optional[TreeNode]) -> List[int]:
+        pass
